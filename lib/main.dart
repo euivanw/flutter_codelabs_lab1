21ac4ccf77d63f1cb3b2a8c7526b6ded9d3d2255
Ivan Wilhelm <ivan.whm@icloud.com>
2020-05-08 19:52:38 -0300

    Lab1: Use um pacote externo

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final parDePalavras = WordPair.random();
     return MaterialApp(
       title: 'Bem-vindo(a) ao Flutter',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Bem-vindo(a) ao Flutter'),
         ),
-        body: const Center(
-          child: const Text('Olá Mundo'),
+        body: Center(
+          child: Text(parDePalavras.asPascalCase),
         ),
       ),
     );
